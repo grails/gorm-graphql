b14ea22510df1297242aa7f4af3f437e3c89e535
Puneet Behl <behl4485@gmail.com>
2020-03-18 21:09:05 +0530

    Updated travis-publish.sh

--- travis-publish.sh
+++ travis-publish.sh
@@ -9,7 +9,7 @@ fi
 
 echo "Publishing for branch $TRAVIS_BRANCH JDK: $TRAVIS_JDK_VERSION"
 
-if [[ $TRAVIS_REPO_SLUG == "grails/gorm-graphql" && $TRAVIS_PULL_REQUEST == 'false' && $TRAVIS_BRANCH =~ ^master|[123]\..\.x$ && $EXIT_STATUS -eq 0 ]]; then
+if [[ $TRAVIS_REPO_SLUG == "grails/gorm-graphql" && $TRAVIS_PULL_REQUEST == 'false' && $EXIT_STATUS -eq 0 ]]; then
 
   echo "Publishing archives"
   export GRADLE_OPTS="-Xmx1500m -Dfile.encoding=UTF-8"
@@ -23,18 +23,16 @@ if [[ $TRAVIS_REPO_SLUG == "grails/gorm-graphql" && $TRAVIS_PULL_REQUEST == 'fal
     if [[ $EXIT_STATUS -eq 0 ]]; then
         ./gradlew bintrayUpload --no-daemon || EXIT_STATUS=$?
     fi
-  else
+  elif [[ $TRAVIS_PULL_REQUEST == 'false' && $TRAVIS_BRANCH =~ ^master|[123]\..\.x$ ]]; then
     # for snapshots only to repo.grails.org
     ./gradlew  publish || EXIT_STATUS=$?
+
+    if [[ $EXIT_STATUS -eq 0 ]]; then
+      echo "Publishing Successful."
+    fi
   fi
 
-  if [[ $EXIT_STATUS -eq 0 ]]; then
-    echo "Publishing Successful."
-  fi
-
-
-  if [[ $EXIT_STATUS -eq 0 ]]; then
-    echo "Publishing Successful."
+  if [[ $EXIT_STATUS -eq 0 && ( -n $TRAVIS_TAG || $TRAVIS_BRANCH =~ ^master$ ) ]]; then
 
     echo "Publishing Documentation..."
     ./gradlew docs:docs
