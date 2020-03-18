82435f0b71f4afd0c1851e832386ecbd2ea16075
Puneet Behl <behl4485@gmail.com>
2020-03-18 21:09:05 +0530

    Updated travis-publish.sh
    to only publish when the branch is master or follow version pattern such as 1.0.x

--- travis-publish.sh
+++ travis-publish.sh
@@ -9,7 +9,7 @@ fi
 
 echo "Publishing for branch $TRAVIS_BRANCH JDK: $TRAVIS_JDK_VERSION"
 
-if [[ $TRAVIS_REPO_SLUG == "grails/gorm-graphql" && $TRAVIS_PULL_REQUEST == 'false' && $EXIT_STATUS -eq 0 ]]; then
+if [[ $TRAVIS_REPO_SLUG == "grails/gorm-graphql" && $TRAVIS_PULL_REQUEST == 'false' && $TRAVIS_BRANCH =~ ^master|[123]\..\.x$ && $EXIT_STATUS -eq 0 ]]; then
 
   echo "Publishing archives"
   export GRADLE_OPTS="-Xmx1500m -Dfile.encoding=UTF-8"
