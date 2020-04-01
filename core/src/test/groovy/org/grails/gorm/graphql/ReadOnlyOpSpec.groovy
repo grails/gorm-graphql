3c39200bb010ba11c95c745629daa29f95a95add
Puneet Behl <behl4485@gmail.com>
2020-04-02 04:22:16 +0530

    #37 Reduce verbosity with Operations
    Add ability to disable:
    * all operations
    * mutation operations
    * query operations

---
+++ core/src/test/groovy/org/grails/gorm/graphql/ReadOnlyOpSpec.groovy
@@ -0,0 +1,39 @@
+package org.grails.gorm.graphql
+
+import grails.gorm.annotation.Entity
+import graphql.schema.GraphQLSchema
+import org.grails.datastore.gorm.GormEntity
+import org.grails.gorm.graphql.entity.dsl.GraphQLMapping
+
+class ReadOnlyOperationsSpec extends HibernateSpec {
+
+    @Override
+    List<Class> getDomainClasses() {
+        [ReadOpOnlyEntity]
+    }
+
+    void "test that only read operation are enabled in clean way"() {
+
+        when:
+        GraphQLSchema schema = new Schema(hibernateDatastore.mappingContext)
+                .generate()
+
+        then:
+        schema.mutationType.fieldDefinitions.isEmpty()
+
+        and:
+        schema.queryType.fieldDefinitions.size() == 3
+        schema.queryType.fieldDefinitions.find {it.name == "readOpOnlyEntity"}
+        schema.queryType.fieldDefinitions.find {it.name == "readOpOnlyEntityList"}
+        schema.queryType.fieldDefinitions.find {it.name == "readOpOnlyEntityCount"}
+    }
+}
+
+@Entity
+class ReadOpOnlyEntity implements GormEntity<ReadOpOnlyEntity> {
+    String prop
+
+    static graphql = GraphQLMapping.build {
+        operations.mutation.enabled false
+    }
+}
