7c32876467b5e5833f6b98d9f35a50a07a94644f
Emma Richardson <49138186+Emrichardsone@users.noreply.github.com>
2023-10-11 08:38:07 -0500

    dependency updates to run integration tests

--- core/src/main/groovy/org/grails/gorm/graphql/entity/property/impl/PersistentGraphQLProperty.groovy
+++ core/src/main/groovy/org/grails/gorm/graphql/entity/property/impl/PersistentGraphQLProperty.groovy
@@ -4,7 +4,6 @@ import grails.gorm.validation.ConstrainedProperty
 import grails.gorm.validation.PersistentEntityValidator
 import graphql.schema.DataFetcher
 import graphql.schema.GraphQLType
-import groovy.transform.CompileStatic
 import org.grails.datastore.mapping.model.MappingContext
 import org.grails.datastore.mapping.model.PersistentEntity
 import org.grails.datastore.mapping.model.PersistentProperty
@@ -33,7 +32,7 @@ import static graphql.schema.GraphQLList.list
  * @author James Kleeh
  * @since 1.0.0
  */
-@CompileStatic
+//@CompileStatic
 class PersistentGraphQLProperty extends OrderedGraphQLProperty {
 
     final Integer order
